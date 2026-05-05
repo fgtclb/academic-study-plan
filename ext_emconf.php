3b0679d87af08e416c6c27bc61d5beb701835623
Stefan Bürk <stefan@buerk.tech>
2026-05-05 16:48:41 +0200

    [TASK] ACE-228: Normalize `ext_emconf.php`
    This changes uses the `ext_emconf.php` normalization of the
    upcoming tool `packwright` to all of the split- and test
    extension files in a good shape.
    
    Used command(s):
    
    ```bash
    pkw-dev extemconf:normalize -t 12 -- packages/**/ext_emconf.php && \
    pkw-dev extemconf:set \
      --ext-version=2.3.2 \
      --author-company='FGTCLB GmbH' \
      --author-email='hello@fgtclb.com' \
      -- packages/**/ext_emconf.php
    ```
    
    > [!NOTE]
    > `pkw (packwraight)` not realease in public yet. It's the upcoming
    > TYPO3 extension release took with a couple of helpers. Currently
    > only `ext_emconf.php` validation, normalization, value setting and
    > constraints handling is availalbe and the reason that the development
    > version has been used.
    
    This is done as preparation for the next release.

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,19 +3,17 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Academic Study Plan',
     'description' => 'TYPO3 extension for building and displaying academic study plans with semesters, modules, and categorization features.',
-    'category' => 'fe',
-    'author' => 'FGTCLB',
-    'author_company' => 'FGTCLB GmbH',
-    'author_email' => 'hello@fgtclb.com',
-    'state' => 'beta',
     'version' => '2.3.2',
+    'category' => 'fe',
+    'state' => 'beta',
+    'author' => 'FGTCLB',
+    'author_email' => 'hello@fgtclb.com',
+    'author_company' => 'FGTCLB GmbH',
     'constraints' => [
         'depends' => [
             'typo3' => '12.0.0-13.4.99',
             'backend' => '12.0.0-13.4.99',
             'academic_base' => '2.3.2',
         ],
-        'conflicts' => [],
-        'suggests' => [],
     ],
 ];
